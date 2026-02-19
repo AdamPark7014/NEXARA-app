c92d609b3b93c34c8c823c95d4f1096eb61a9001
Adam Park <tu-correo@ejemplo.com>
2026-02-19 13:43:04 -0600

    Update favicon to Nexara logo

--- apps/web/app/head.tsx
+++ apps/web/app/head.tsx
@@ -1,10 +1,8 @@
 export default function Head() {
   return (
     <>
-      <title>Nexara</title>
-      <link rel="icon" href="/logo-nexara.png" sizes="any" />
+      <link rel="icon" href="/logo-nexara.png" type="image/png" />
       <link rel="apple-touch-icon" href="/logo-nexara.png" />
-      <link rel="shortcut icon" href="/logo-nexara.png" />
     </>
   );
 }
