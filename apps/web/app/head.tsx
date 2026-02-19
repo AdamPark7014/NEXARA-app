f305645290a7f9f3cad13a43ec562a94611c352a
Adam Park <tu-correo@ejemplo.com>
2026-02-19 13:34:03 -0600

    Add Nexara favicon links

---
+++ apps/web/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Nexara</title>
+      <link rel="icon" href="/logo-nexara.png" sizes="any" />
+      <link rel="apple-touch-icon" href="/logo-nexara.png" />
+      <link rel="shortcut icon" href="/logo-nexara.png" />
+    </>
+  );
+}
