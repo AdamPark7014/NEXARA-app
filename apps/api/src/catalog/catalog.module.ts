c19ce2a955d81f31148e3bdd1994a0d30b4f4fbc
AdamPark7014 <ad.pozo.ont@gmail.com>
2026-05-25 00:09:02 -0600

    fix(api): import PrismaModule in catalog module

--- apps/api/src/catalog/catalog.module.ts
+++ apps/api/src/catalog/catalog.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module.js';
 import { CatalogController } from './catalog.controller.js';
 import { CatalogService } from './catalog.service.js';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CatalogController],
   providers: [CatalogService],
   exports: [CatalogService],
